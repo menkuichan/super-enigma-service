b1758427c973c965bc481acd468368f3cefb1888
MenkuiChan <ilonaspolding@gmail.com>
2020-01-28 16:54:50 +0300

    Add error handling for controllers (#11)
    * add error handling for controllers
    
    * fix error handling
    
    * refactor error handling
    
    * fix error handling

--- src/controllers/genre.js
+++ src/controllers/genre.js
@@ -1,22 +1,30 @@
 const { Genre } = require('../models/genre');
 
-const getGenres = async (ctx, next) => {
-  const genres = await Genre.find();
+const getGenres = async (ctx) => {
+  try {
+    const genres = await Genre.find();
 
-  ctx.status = 200;
-  ctx.body = genres;
+    ctx.status = 200;
+    ctx.body = genres;
+  } catch (error) {
+    ctx.status = 500;
+    ctx.body = error.message;
+  }
+};
 
-  next();
-}
+const getGenreById = async (ctx) => {
+  try {
+    const { id } = ctx.params;
+    const genre = await Genre.findOne({ id });
 
-const getGenreById = async (ctx, next) => {
-  const { id } = ctx.params;
-  const genre = await Genre.findOne({ id });
+    ctx.assert(genre, 404, 'Genre not found');
 
-  ctx.status = 200;
-  ctx.body = genre;
-
-  next();
-}
+    ctx.status = 200;
+    ctx.body = genre;
+  } catch (error) {
+    ctx.status = error.status || 500;
+    ctx.body = error.message;
+  }
+};
 
 module.exports = { getGenres, getGenreById };
--- src/controllers/movie.js
+++ src/controllers/movie.js
@@ -1,26 +1,34 @@
 const { Movie } = require('../models/movie');
 
-const getMovies = async (ctx, next) => {
-  const { page, perPage } = ctx.query;
+const getMovies = async (ctx) => {
+  try {
+    const { page, perPage } = ctx.query;
 
-  const movies = await Movie.find()
-    .skip((page * perPage) - perPage)
-    .limit(+perPage);
+    const movies = await Movie.find()
+      .skip((page * perPage) - perPage)
+      .limit(+perPage);
 
-  ctx.status = 200;
-  ctx.body = movies;
+    ctx.status = 200;
+    ctx.body = movies;
+  } catch (error) {
+    ctx.status = 500;
+    ctx.body = error.message;
+  }
+};
 
-  next();
-}
-
-const getMovieById = async (ctx, next) => {
+const getMovieById = async (ctx) => {
   const { id } = ctx.params;
-  const movie = await Movie.findOne({ id });
+  try {
+    const movie = await Movie.findOne({ id });
 
-  ctx.status = 200;
-  ctx.body = movie;
+    ctx.assert(movie, 404, 'Movie not found');
 
-  next();
-}
+    ctx.status = 200;
+    ctx.body = movie;
+  } catch (error) {
+    ctx.status = error.status || 500;
+    ctx.body = error.message;
+  }
+};
 
 module.exports = { getMovies, getMovieById };
